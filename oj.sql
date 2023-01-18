34aed2cb3d8de45a36ebaece83c2ad975baee7e5
lmy <longmy5@mail2.sysu.edu.cn>
2023-01-19 02:12:43 +0800

    修改数据库

--- oj.sql
+++ oj.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80030
+ Source Server Version : 80018
  Source Host           : localhost:3306
  Source Schema         : oj
 
  Target Server Type    : MySQL
- Target Server Version : 80030
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 18/01/2023 17:39:17
+ Date: 19/01/2023 02:12:17
 */
 
 SET NAMES utf8mb4;
@@ -22,17 +22,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `answer_info`;
 CREATE TABLE `answer_info`  (
-  `answerId` int NOT NULL AUTO_INCREMENT,
-  `contestId` int NOT NULL,
-  `questionId` int NOT NULL,
+  `answerId` int(11) NOT NULL AUTO_INCREMENT,
+  `contestId` int(11) NOT NULL,
+  `questionId` int(11) NOT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `answerTime` datetime NULL DEFAULT NULL,
+  `answerContent` varchar(15000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `result` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `runTime` float NULL DEFAULT NULL,
   PRIMARY KEY (`answerId`) USING BTREE,
-  INDEX `contestId`(`contestId` ASC) USING BTREE,
-  INDEX `questionId`(`questionId` ASC) USING BTREE,
-  INDEX `username`(`username` ASC) USING BTREE,
+  INDEX `contestId`(`contestId`) USING BTREE,
+  INDEX `questionId`(`questionId`) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
   CONSTRAINT `answer_info_ibfk_1` FOREIGN KEY (`contestId`) REFERENCES `contest_info` (`contestId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `answer_info_ibfk_2` FOREIGN KEY (`questionId`) REFERENCES `question_info` (`questionId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `answer_info_ibfk_3` FOREIGN KEY (`username`) REFERENCES `user_info` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
@@ -41,20 +42,20 @@ CREATE TABLE `answer_info`  (
 -- ----------------------------
 -- Records of answer_info
 -- ----------------------------
-INSERT INTO `answer_info` VALUES (1, 1, 2, 'aa12', '2023-01-27 20:29:34', 'success', 220);
+INSERT INTO `answer_info` VALUES (1, 1, 2, 'aa12', '2023-01-27 20:29:34', NULL, 'success', 220);
 
 -- ----------------------------
 -- Table structure for contest_info
 -- ----------------------------
 DROP TABLE IF EXISTS `contest_info`;
 CREATE TABLE `contest_info`  (
-  `contestId` int NOT NULL,
-  `contestName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contestId` int(11) NOT NULL,
+  `contestTitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `startTime` datetime NULL DEFAULT NULL,
   `endTime` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`contestId`) USING BTREE,
-  INDEX `username`(`username` ASC) USING BTREE,
+  INDEX `username`(`username`) USING BTREE,
   CONSTRAINT `contest_info_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user_info` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
@@ -63,16 +64,18 @@ CREATE TABLE `contest_info`  (
 -- ----------------------------
 INSERT INTO `contest_info` VALUES (1, 'test', '111', '2022-12-28 20:25:40', '2023-01-12 20:25:42');
 INSERT INTO `contest_info` VALUES (2, 'abc', '111', '2022-12-29 20:25:54', '2023-01-08 20:25:58');
+INSERT INTO `contest_info` VALUES (3, 'test24', '111', '2002-02-01 00:00:00', '2002-02-19 00:00:00');
+INSERT INTO `contest_info` VALUES (904, '67567', '111', '2002-02-01 00:00:00', '2002-02-19 00:00:00');
 
 -- ----------------------------
 -- Table structure for contest_question
 -- ----------------------------
 DROP TABLE IF EXISTS `contest_question`;
 CREATE TABLE `contest_question`  (
-  `contestId` int NOT NULL,
-  `questionId` int NOT NULL,
-  INDEX `contestId`(`contestId` ASC) USING BTREE,
-  INDEX `questionId`(`questionId` ASC) USING BTREE,
+  `contestId` int(11) NOT NULL,
+  `questionId` int(11) NOT NULL,
+  INDEX `contestId`(`contestId`) USING BTREE,
+  INDEX `questionId`(`questionId`) USING BTREE,
   CONSTRAINT `contest_question_ibfk_1` FOREIGN KEY (`contestId`) REFERENCES `contest_info` (`contestId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `contest_question_ibfk_2` FOREIGN KEY (`questionId`) REFERENCES `question_info` (`questionId`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
@@ -89,7 +92,7 @@ INSERT INTO `contest_question` VALUES (2, 4);
 -- ----------------------------
 DROP TABLE IF EXISTS `question_info`;
 CREATE TABLE `question_info`  (
-  `questionId` int NOT NULL,
+  `questionId` int(11) NOT NULL,
   `questionTitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `questionContent` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `questionExample` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
@@ -111,7 +114,7 @@ DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info`  (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `isManager` int NULL DEFAULT NULL,
+  `isManager` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`username`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
